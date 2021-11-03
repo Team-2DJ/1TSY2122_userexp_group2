72165088311035a57ae093c1b7868c165266afef
David Gutierrez <davidandrew.gutierrez@benilde.edu.ph>
2021-11-03 20:27:45 +0800

    UI scaling now follows the resolution (not free scale basically), added in one powerup and one type of special brick

--- Brick Breaker Redux/Assets/Scripts/BrickScript.cs
+++ Brick Breaker Redux/Assets/Scripts/BrickScript.cs
@@ -5,4 +5,15 @@ using UnityEngine;
 public class BrickScript : MonoBehaviour
 {
     public int points;
+    public int hitsToBreak;
+    public Sprite hitSprite;
+
+    public void BreakBrick()
+    {
+        hitsToBreak--;
+
+        // change the sprite & its color when object is hit (might make it an if statement in the future (David) 
+        this.GetComponent<SpriteRenderer>().color = new Color(255f / 255f, 152f / 255f, 66f / 255f);
+        this.GetComponent<SpriteRenderer>().sprite = hitSprite;
+    }
 }
